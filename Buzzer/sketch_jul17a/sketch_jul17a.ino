01e4964c20def6f96966e70a81fa36fe5fbdf026
teodora27 <cojocaruteodoralaura@yahoo.com>
2024-07-18 11:23:03 +0300

    buzzer script commit

--- Buzzer/sketch_jul17a/sketch_jul17a.ino
+++ Buzzer/sketch_jul17a/sketch_jul17a.ino
@@ -1,24 +1,45 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+const char* ssid = "UPB-Guest";
+const char* password = NULL;
 const int buzzerPin = 14; // GPIO pin connected to the buzzer I/O pin
-bool ok;
+
+WebServer server(80);
 
 void setup() {
+  Serial.begin(115200);
   pinMode(buzzerPin, OUTPUT); // Set the buzzer pin as an output
-  ok=true;
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("WiFi connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/alert", handleAlert);
+  server.begin();
 }
 
 void loop() {
-  // Test sound for correct and incorrect actions
-  
-  //aici trebuie sa primim date de la camera
+  server.handleClient();
+}
 
-  if(ok)
-  {
-    playCorrectSound();
-    delay(2000);
-  } // 2-second pause between sounds for testing
+void handleAlert() {
+  Serial.println("Face detected! Activating buzzer...");
+  playCorrectSound();
+  server.send(200, "text/plain", "Buzzer activated");
 }
 
 void playCorrectSound() {
-  tone(buzzerPin, 1000, 30); // First tone, 1000 Hz for 30 ms
-  noTone(buzzerPin);
+  tone(buzzerPin, 1000); // Start tone at 1000 Hz
+  delay(1000); // Wait for 1 second
+  noTone(buzzerPin); // Stop the tone
 }
